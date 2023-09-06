4f68e9b80183f7a36e59785ff14e345e8b81981f
gaetano.erizian <gaetano.erizian@gmail.com>
2023-09-06 13:14:15 +0200

    Init project

---
+++ src/main/java/com/techstone/carbroker/CarbrokerApplication.java
@@ -0,0 +1,13 @@
+package com.techstone.carbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarbrokerApplication.class, args);
+    }
+
+}
